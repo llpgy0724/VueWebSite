27bdbaf63ffc8a62e623cf9e6b297c2051732a9a
PangHu77 <2591508117@qq.com>
2017-11-03 15:19:24 +0800

    vue project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Loader :isshow="getShowLoading" loaderbackground="rgba(0,0,0,0.3)"></Loader>
+  </div>
+</template>
+
+<script>
+import Loader from './components/common/loader/loader'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      getShowLoading: true
+    }
+  },
+  components: {
+    Loader
+  }
+}
+</script>
+
+<style>
+
+</style>
